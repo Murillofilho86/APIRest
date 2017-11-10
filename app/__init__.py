cc742e875138854a6ecb60e08939936adef72e23
Murillo Filho <murillohard@hotmail.com>
2017-11-09 23:28:15 -0200

    Inital Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/storage.db'
+db = SQLAkchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+
+db.create_all()
