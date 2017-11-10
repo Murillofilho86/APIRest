43faba74cd91df010f62ecaacc8dd2cf1588201e
Murillo Filho <murillohard@hotmail.com>
2017-11-09 23:43:02 -0200

    Criação do banco de dados e Gerenciamento da API

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restless import APIManager
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/storage.db'
-db = SQLAkchemy(app)
+_db = SQLAlchemy(app)
+
+'''
+#Classe para criar tabela usuarios
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
 
 db.create_all()
+'''
+
+#Gerenciado da API 	(app,          banco de dados)
+manager = APIManager(app, flask_sqlalchemy_db=_db)
